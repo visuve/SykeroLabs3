6316d9a60cb82e689c6e18ba8f10d8c9643eefc8
visuve <veli-matti.visuri@iki.fi>
2024-02-10 00:07:03 +0200

    Remove libgpiod
    - The library is useless as most things are just ioctl calls

--- src/sykerolabs.cpp
+++ src/sykerolabs.cpp
@@ -1,10 +1,4 @@
-// https://github.com/brgl/libgpiod/tree/v1.6.x/bindings/cxx/examples
-// https://framagit.org/cpb/example-programs-using-libgpiod/-/tree/master
-
-// Just to fool Visual Studio Intellisense
-#ifndef RASPBERRY
 #include "mega.pch"
-#endif
 
 namespace sl
 {
@@ -12,11 +6,11 @@ namespace sl
 	{
 		enum : uint8_t
 		{
-			NFT_PUMP_1 = 5,
-			NFT_PUMP_2 = 6,
+			RESERVOIR_PUMP_1 = 5,
+			RESERVOIR_PUMP_2 = 6,
+			NFT_PUMP_1 = 13,
+			NFT_PUMP_2 = 16,
 			FAN_PWM_SIGNAL = 12,
-			RESERVOIR_PUMP_1 = 13,
-			RESERVOIR_PUMP_2 = 16,
 			FAN_1_RELAY = 19,
 			FAN_2_RELAY = 20,
 			FAN_1_TACHOMETER = 23,
@@ -31,104 +25,183 @@ namespace sl
 		signaled = signal;
 	}
 
-	bool compare_event_time(
-		const gpiod::line_event& lhs, 
-		const gpiod::line_event& rhs)
+	class gpio_descriptor
 	{
-		return lhs.timestamp < rhs.timestamp;
-	}
+	public:
+		constexpr gpio_descriptor(int descriptor) :
+			_descriptor(descriptor)
+		{
+		}
 
-	float rpm(gpiod::line& line)
+		inline virtual ~gpio_descriptor()
+		{
+			if (_descriptor > 0)
+			{
+				close(_descriptor);
+			}
+		}
+
+		template<typename... Args>
+		int ioctl(uint32_t request, Args... args) const
+		{
+			int result = ::ioctl(_descriptor, request, args...);
+
+			if (result == -1)
+			{
+				throw std::system_error(errno, std::system_category(), "ioctl");
+			}
+
+			return result;
+		}
+
+		operator int() const
+		{
+			return _descriptor;
+		}
+
+	protected:
+		int _descriptor = 0;
+	};
+
+	struct gpio_data
 	{
-		line.event_wait(std::chrono::seconds(10));
+		uint32_t offset = 0;
+		bool value = true;
+	};
 
-		// Sigh; this is a blocking function. I need to move this to a thread
-		// as gpiod does not appear to have async functions anywhere
-		std::vector<gpiod::line_event> events = line.event_read_multiple();
+	template <size_t N>
+	class gpio_line_group : private gpio_descriptor
+	{
+	public:
+		constexpr gpio_line_group(int descriptor, const uint32_t(&offsets)[N]) :
+			gpio_descriptor(descriptor)
+		{
+			clone(_offsets, offsets);
+		}
 
-		// https://github.com/brgl/libgpiod/blob/v1.6.x/bindings/cxx/line.cpp#L248
-		assert(events.size() == 16); 
+		void read(std::array<gpio_data, N>& data)
+		{
+			gpio_v2_line_values values = { 0 };
 
-		auto older = std::min_element(events.cbegin(), events.cend(), compare_event_time);
-		auto newer = std::max_element(events.cbegin(), events.cend(), compare_event_time);
+			for (size_t i = 0; i < N; ++i)
+			{
+				values.mask |= (1ull << i);
+			}
 
-		std::chrono::nanoseconds diff = newer->timestamp - older->timestamp;
+			ioctl(GPIO_V2_LINE_GET_VALUES_IOCTL, &values);
 
-		float revolutions_per_nanosecond = 16.0f / diff.count();
+			for (size_t i = 0; i < N; ++i)
+			{
+				data[i].offset = _offsets[i];
+				data[i].value = values.bits & (1ull << i);
+			}
+		}
 
-		constexpr float nanoseconds_in_minute = 
-			std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::minutes(1)).count();
+		void write(const std::array<gpio_data, N>& data)
+		{
+			gpio_v2_line_values values = { 0 };
 
-		return revolutions_per_nanosecond * nanoseconds_in_minute;
-	}
+			for (size_t i = 0; i < N; ++i)
+			{
+				values.mask |= (1ull << i);
+				values.bits |= (uint64_t(data[i].value) << i);
+			}
+
+			ioctl(GPIO_V2_LINE_SET_VALUES_IOCTL, &values);
+		}
+
+	private:
+		uint32_t _offsets[N];
+	};
+
+	// https://github.com/torvalds/linux/blob/master/tools/gpio/lsgpio.c
+	// https://github.com/torvalds/linux/blob/master/tools/gpio/gpio-utils.c
+	class gpio_chip : public gpio_descriptor
+	{
+	public:
+		inline gpio_chip(std::string_view name) :
+			gpio_descriptor(open(name.data(), O_RDONLY))
+		{
+			if (_descriptor < 0)
+			{
+				throw std::system_error(errno, std::system_category(), name.data());
+			}
+		}
+
+		template <size_t N>
+		gpio_line_group<N> lines(uint64_t flags, const uint32_t(&offsets)[N]) const
+		{
+			gpio_v2_line_config config = { 0 };
+			config.flags = flags;
+
+			gpio_v2_line_request request = { 0 };
+			request.config = config;
+			request.num_lines = N;
+
+			clone(request.offsets, offsets);
+			clone(request.consumer, "sykerolabs");
+
+			gpio_descriptor::ioctl(GPIO_V2_GET_LINE_IOCTL, &request);
+
+			return gpio_line_group<N>(request.fd, offsets);
+		}
+	};
 
 	int run()
 	{
-		constexpr char chip_name[] = "gpiochip4";
-
-		gpiod::chip chip(chip_name);
-
-		const std::vector<uint32_t> input_pins =
+		const uint32_t input_pins[] =
 		{
-			pins::NFT_PUMP_1,
-			pins::NFT_PUMP_2,
 			pins::RESERVOIR_PUMP_1,
 			pins::RESERVOIR_PUMP_2
 		};
 
-		const std::vector<uint32_t> output_pins =
+		const uint32_t output_pins[] =
 		{
+			pins::NFT_PUMP_1,
+			pins::NFT_PUMP_2,
 			pins::FAN_1_RELAY,
 			pins::FAN_2_RELAY
 		};
 
-		const std::vector<uint32_t> interrupt_pins =
+		const uint32_t interrupt_pins[] =
 		{
 			pins::FAN_1_TACHOMETER,
 			pins::FAN_2_TACHOMETER
 		};
 
-		const size_t interrupt_pin_count = sizeof(interrupt_pins) / sizeof(uint32_t);
+		constexpr char chip_name[] = "/dev/gpiochip4";
 
-		gpiod::line_bulk inputs = chip.get_lines(input_pins);
-		inputs.request({ "sykerolabs_in", gpiod::line_request::DIRECTION_INPUT, 0 });
+		gpio_chip chip(chip_name);
 
-		gpiod::line_bulk outputs = chip.get_lines(output_pins);
-		outputs.request({ "sykerolabs_out", gpiod::line_request::DIRECTION_OUTPUT, 0 });
+		gpio_line_group input_lines =
+			chip.lines(GPIO_V2_LINE_FLAG_INPUT, input_pins);
 
-		gpiod::line_bulk interrupts = chip.get_lines(interrupt_pins);
-		interrupts.request({ "sykerolabs_irq", gpiod::line_request::EVENT_RISING_EDGE, 0 });
+		gpio_line_group output_lines =
+			chip.lines(GPIO_V2_LINE_FLAG_OUTPUT, output_pins);
 
 		uint64_t t = 0;
 
 		while (!signaled)
 		{
-			std::cout << "\nPIM " << ++t << ": \n";
+			std::cout << "\nT=" << ++t << ": \n";
 
-			inputs.event_wait(std::chrono::seconds(1));
+			std::array<gpio_data, 2> input_data;
+			input_lines.read(input_data);
 
-			std::vector<int> values = inputs.get_values();
+			for (auto& input : input_data)
+			{
+				std::cout << input.offset << '=' << input.value << '\n';
+			}
 
-			assert(values.size() == 4);
+			std::array<gpio_data, 4> output_data =
+			{
+				gpio_data(pins::NFT_PUMP_1, t % 4 == 0),
+				gpio_data(pins::NFT_PUMP_2, t % 4 == 1),
+				gpio_data(pins::FAN_1_RELAY, t % 4 == 2),
+				gpio_data(pins::FAN_2_RELAY, t % 4 == 3)
+			};
 
-			size_t i = static_cast<size_t>(-1);
-
-			std::cout << "NFT pump 1 relay = " << !values[++i] << '\n';
-			std::cout << "NFT pump 2 relay = " << !values[++i] << '\n';
-
-			std::cout << "Pool pump 1 relay = " << !values[++i] << '\n';
-			std::cout << "Pool pump 2 relay = " << !values[++i] << '\n';
-
-			int fan1_value = t % 10 == 0;
-			int fan2_value = t % 10 != 0;
-
-			outputs.set_values({ fan1_value, fan2_value });
-
-			std::cout << "Fan 1 relay = " << fan1_value << '\n';
-			std::cout << "Fan 2 relay = " << fan2_value << '\n';
-
-			//std::cout << "Fan 1 RPM = " << rpm(interrupts.get(0)) << '\n';
-			//std::cout << "Fan 2 RPM = " << rpm(interrupts.get(1)) << '\n';
+			output_lines.write(output_data);
 
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
@@ -143,11 +216,12 @@ int main()
 
 	try
 	{
+		std::cout << "Starting...\n";
 		return sl::run();
 	}
-	catch (const std::exception& e)
+	catch (const std::system_error& e)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cout << e.what() << std::endl;
 	}
 
 	return -1;
