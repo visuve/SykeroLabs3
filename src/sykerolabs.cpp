31cfcfad8b06a8e5f6971a3822607ecd30ad97a4
visuve <veli-matti.visuri@iki.fi>
2024-02-10 10:58:52 +0200

    Rename gpio_descriptor to raii_descriptor & add methods

--- src/sykerolabs.cpp
+++ src/sykerolabs.cpp
@@ -25,15 +25,24 @@ namespace sl
 		signaled = signal;
 	}
 
-	class gpio_descriptor
+	class raii_descriptor
 	{
 	public:
-		constexpr gpio_descriptor(int descriptor) :
+		raii_descriptor(int descriptor) :
 			_descriptor(descriptor)
+		{
+			if (_descriptor < 0)
+			{
+				throw std::system_error(errno, std::system_category(), "open");
+			}
+		}
+
+		raii_descriptor(std::string_view path, int mode = O_RDONLY) :
+			raii_descriptor(open(path.data(), mode))
 		{
 		}
 
-		inline virtual ~gpio_descriptor()
+		inline virtual ~raii_descriptor()
 		{
 			if (_descriptor > 0)
 			{
@@ -41,12 +50,51 @@ namespace sl
 			}
 		}
 
+		bool read(void* data, size_t size) const
+		{
+			int result = ::read(_descriptor, data, size);
+
+			if (result < 0)
+			{
+				if (errno == -EAGAIN)
+				{
+					return false; // No data
+				}
+
+				throw std::system_error(errno, std::system_category(), "read");
+			}
+
+			if (result != size)
+			{
+				throw std::system_error(-EIO, std::system_category(), "read");
+			}
+
+			return true;
+		}
+
+		template <typename T>
+		bool read_value(T& t) const
+		{
+			return raii_descriptor::read(&t, sizeof(T));
+		}
+
+		void lseek(off_t offset, int whence) const
+		{
+			int result = ::lseek(_descriptor, offset, whence);
+
+			if (result < 0)
+			{
+				throw std::system_error(errno, std::system_category(), "lseek");
+			}
+		}
+
+	protected:
 		template<typename... Args>
 		int ioctl(uint32_t request, Args... args) const
 		{
 			int result = ::ioctl(_descriptor, request, args...);
 
-			if (result == -1)
+			if (result < 0)
 			{
 				throw std::system_error(errno, std::system_category(), "ioctl");
 			}
@@ -54,12 +102,21 @@ namespace sl
 			return result;
 		}
 
-		operator int() const
+		void poll(std::chrono::milliseconds timeout) const
 		{
-			return _descriptor;
+			pollfd poll_descriptor;
+			poll_descriptor.fd = _descriptor;
+			poll_descriptor.events = POLLPRI | POLLERR;
+			poll_descriptor.revents = 0;
+
+			int result = ::poll(&poll_descriptor, 1, timeout.count());
+
+			if (result < 0)
+			{
+				throw std::system_error(errno, std::system_category(), "poll");
+			}
 		}
 
-	protected:
 		int _descriptor = 0;
 	};
 
@@ -69,17 +126,16 @@ namespace sl
 		bool value = true;
 	};
 
-	class gpio_line_group : private gpio_descriptor
+	class gpio_line_group : private raii_descriptor
 	{
 	public:
-
 		gpio_line_group(int descriptor, const std::set<uint32_t>& offsets) :
-			gpio_descriptor(descriptor),
+			raii_descriptor(descriptor),
 			_offsets(offsets)
 		{
 		}
 
-		void read(std::span<gpio_lvp> data) const
+		void read_values(std::span<gpio_lvp> data) const
 		{
 			assert(data.size() <= _offsets.size());
 
@@ -93,7 +149,7 @@ namespace sl
 			}
 
 			gpio_v2_line_values values = { 0, mask.to_ullong() };
-			ioctl(GPIO_V2_LINE_GET_VALUES_IOCTL, &values);
+			raii_descriptor::ioctl(GPIO_V2_LINE_GET_VALUES_IOCTL, &values);
 			bits = values.bits;
 
 			for (gpio_lvp& lvp : data)
@@ -103,14 +159,14 @@ namespace sl
 			}
 		}
 
-		void readz(gpio_lvp& lvp) const
+		void read_value(gpio_lvp& lvp) const
 		{
 			gpio_lvp data[] = { lvp };
-			read(data);
+			read_values(data);
 			lvp = data[0];
 		}
 
-		void write(std::span<gpio_lvp> data) const
+		void write_values(std::span<gpio_lvp> data) const
 		{
 			assert(data.size() <= _offsets.size());
 
@@ -125,13 +181,21 @@ namespace sl
 			}
 
 			gpio_v2_line_values values = { bits.to_ullong(), mask.to_ullong() };
-			ioctl(GPIO_V2_LINE_SET_VALUES_IOCTL, &values);
+			raii_descriptor::ioctl(GPIO_V2_LINE_SET_VALUES_IOCTL, &values);
 		}
 
-		void writez(const gpio_lvp& lvp) const
+		// https://github.com/torvalds/linux/blob/master/tools/gpio/gpio-event-mon.c
+		gpio_v2_line_event read_event() const
+		{
+			gpio_v2_line_event event = { 0 };
+			raii_descriptor::read_value(event);
+			return event;
+		}
+
+		void write_value(const gpio_lvp& lvp) const
 		{
 			gpio_lvp data[] = { lvp };
-			write(data);
+			write_values(data);
 		}
 
 	private:
@@ -152,11 +216,11 @@ namespace sl
 
 	// https://github.com/torvalds/linux/blob/master/tools/gpio/lsgpio.c
 	// https://github.com/torvalds/linux/blob/master/tools/gpio/gpio-utils.c
-	class gpio_chip : public gpio_descriptor
+	class gpio_chip : public raii_descriptor
 	{
 	public:
 		inline gpio_chip(std::string_view name) :
-			gpio_descriptor(open(name.data(), O_RDONLY))
+			raii_descriptor(name)
 		{
 			if (_descriptor < 0)
 			{
@@ -183,7 +247,7 @@ namespace sl
 			
 			clone(request.consumer, "sykerolabs");
 
-			gpio_descriptor::ioctl(GPIO_V2_GET_LINE_IOCTL, &request);
+			raii_descriptor::ioctl(GPIO_V2_GET_LINE_IOCTL, &request);
 
 			return gpio_line_group(request.fd, offsets);
 		}
@@ -205,7 +269,7 @@ namespace sl
 			pins::FAN_2_RELAY
 		};
 
-		const std::set<uint32_t> interrupt_pins =
+		const std::set<uint32_t> monitor_pins =
 		{
 			pins::FAN_1_TACHOMETER,
 			pins::FAN_2_TACHOMETER
@@ -221,11 +285,14 @@ namespace sl
 		gpio_line_group output_lines =
 			chip.lines(GPIO_V2_LINE_FLAG_OUTPUT, output_pins);
 
+		//gpio_line_group monitor_lines =
+		//	chip.lines(GPIO_V2_LINE_FLAG_EDGE_RISING, monitor_pins);
+
 		uint64_t t = 0;
 
-		std::ifstream thermal_zone0("/sys/class/thermal/thermal_zone0/temp");
+		raii_descriptor thermal_zone0("/sys/class/thermal/thermal_zone0/temp");
 
-		float temperature = 0.0;
+		std::string temperature(5, '\0');
 
 		while (!signaled)
 		{
@@ -237,7 +304,7 @@ namespace sl
 				gpio_lvp(pins::RESERVOIR_PUMP_2)
 			};
 
-			input_lines.read(input_data);
+			input_lines.read_values(input_data);
 
 			for (auto& input : input_data)
 			{
@@ -252,12 +319,12 @@ namespace sl
 				gpio_lvp(pins::FAN_2_RELAY, t % 4 == 3)
 			};
 
-			output_lines.write(output_data);
+			output_lines.write_values(output_data);
 
-			thermal_zone0 >> temperature;
-			thermal_zone0.seekg(0, std::ios::beg);
+			thermal_zone0.read(temperature.data(), temperature.size());
+			thermal_zone0.lseek(0, SEEK_SET);
 
-			std::cout << "CPU @ " << temperature / 1000 << "c\n";
+			std::cout << "CPU @ " << std::stof(temperature) / 1000.0f << "c\n";
 
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
