9b2e412b682b9b581d631db87529fcba27932fc9
visuve <veli-matti.visuri@iki.fi>
2024-02-29 00:42:31 +0200

    Use localtime_r instead of localtime

--- src/sykero_time.cpp
+++ src/sykero_time.cpp
@@ -1,39 +1,41 @@
 #include "mega.pch"
 #include "sykero_time.hpp"
+#include "sykero_mem.hpp"
 
 namespace sl::time
 {
-	std::tm* local_time(const std::chrono::system_clock::time_point& time_point)
+	std::tm local_time(const std::chrono::system_clock::time_point& time_point)
 	{
 		std::time_t tt = std::chrono::system_clock::to_time_t(time_point);
 
-		std::tm* tm = std::localtime(&tt);
+		std::tm tm;
+		mem::clear(tm);
 
-		if (!tm)
+		if (!localtime_r(&tt, &tm))
 		{
 			throw std::runtime_error("std::localtime failed");
 		}
 
-		assert(tm->tm_hour >= 0 && tm->tm_hour < 24);
+		assert(tm.tm_hour >= 0 && tm.tm_hour < 24);
 
 		return tm;
 	}
 
 	bool is_night(const std::chrono::system_clock::time_point& time_point)
 	{
-		std::tm* tm = local_time(time_point);
-		return tm->tm_hour >= 22 || tm->tm_hour <= 8;
+		std::tm tm = local_time(time_point);
+		return tm.tm_hour >= 22 || tm.tm_hour <= 8;
 	}
 
 	std::string to_string(const std::chrono::system_clock::time_point& time_point)
 	{
-		std::tm* tm = local_time(time_point);
+		std::tm tm = local_time(time_point);
 
 		constexpr size_t time_stamp_size = sizeof("2024-02-28T16:45:18");
 
 		char time_stamp[time_stamp_size];
 
-		if (std::strftime(time_stamp, time_stamp_size, "%FT%T", tm) != time_stamp_size - 1)
+		if (std::strftime(time_stamp, time_stamp_size, "%FT%T", &tm) != time_stamp_size - 1)
 		{
 			throw std::runtime_error("std::strftime failed");
 		}
