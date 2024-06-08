4ed6eb999c50bf640b86edfa5de0f24c91bceb76
visuve <veli-matti.visuri@iki.fi>
2024-06-08 08:52:20 +0300

    Log build type on start

--- src/sykero_log.cpp
+++ src/sykero_log.cpp
@@ -11,13 +11,18 @@ namespace sl::log
 
 #ifdef NDEBUG
 		setlogmask(LOG_UPTO(LOG_INFO));
+		constexpr char BUILD_TYPE[] = "release";
 #else
 		setlogmask(LOG_UPTO(LOG_DEBUG));
+		constexpr char BUILD_TYPE[] = "debug";
 #endif
 		openlog("sykerolabs", LOG_CONS | LOG_PID | LOG_NDELAY, facility);
 
+		syslog(LOG_INFO, "build date: %s", __DATE__);
+		syslog(LOG_INFO, "build time: %s", __TIME__);
+		syslog(LOG_INFO, "build type: %s", BUILD_TYPE);
+
 		syslog(LOG_INFO, "started from %s", path);
-		syslog(LOG_INFO, "build date: %s time: %s", __DATE__, __TIME__);
 	}
 
 	facility::~facility()
