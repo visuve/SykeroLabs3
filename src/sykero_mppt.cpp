2c5950d4d8899a78d699c882588d89ce42760845
visuve <veli-matti.visuri@iki.fi>
2026-06-09 21:31:48 +0300

    Add sykero_mppt
    - I bought a new solar panel controller and it has some nice data collection features via an 4 pin RS232 interface

---
+++ src/sykero_mppt.cpp
@@ -0,0 +1,250 @@
+#include "mega.pch"
+#include "sykero_mppt.hpp"
+#include "sykero_log.hpp"
+
+namespace sl::mppt
+{
+	constexpr size_t MAX_BUFFER_SIZE = 256;
+	constexpr size_t MAX_STRING_LENGTH = 32;
+
+	constexpr char DELIM_LF = '\n';
+	constexpr char DELIM_CR = '\r';
+	constexpr char DELIM_TAB = '\t';
+
+	constexpr char KEY_CHECKSUM[] = "Checksum";
+
+	controller::controller(const std::filesystem::path& path) :
+		io::file_descriptor(path, O_RDWR | O_NOCTTY | O_NDELAY),
+		_properties(
+		{
+			{ "V", &battery_voltage },
+			{ "I", &battery_current},
+			{ "VPV", &panel_voltage },
+			{ "PPV", &panel_power},
+			{ "IL", &load_current },
+			{ "CS", &state },
+			{ "ERR", &error },
+			{ "MPPT", &tracker_mode},
+			{ "H19", &yield_total},
+			{ "H21", &max_power_today }
+		})
+	{
+		_key.reserve(MAX_STRING_LENGTH);
+		_value.reserve(MAX_STRING_LENGTH);
+
+		struct termios options;
+		mem::clear(options);
+
+		if (ioctl(TCGETS, &options) < 0)
+		{
+			throw std::runtime_error("failed to get termios");
+		}
+
+		cfsetispeed(&options, B19200);
+		cfsetospeed(&options, B19200);
+
+		options.c_cflag |= (CLOCAL | CREAD | CS8);
+		options.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
+		options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+		options.c_oflag &= ~OPOST;
+
+		if (ioctl(TCSETS, &options) < 0)
+		{
+			throw std::runtime_error("failed to set termios");
+		}
+	}
+
+	void controller::update()
+	{
+		std::array<uint8_t, MAX_BUFFER_SIZE> chunk;
+
+		while (poll(std::chrono::milliseconds(0), POLLIN))
+		{
+			size_t bytes_read = read(chunk.data(), chunk.size());
+
+			if (bytes_read <= 0)
+			{
+				break;
+			}
+
+			for (size_t i = 0; i < bytes_read; ++i)
+			{
+				switch (advance(chunk[i]))
+				{
+					case frame_event::PENDING:
+					{
+						continue;
+					}
+					case frame_event::PAIR_READY:
+					{
+						parse_pair();
+						break;
+					}
+					case frame_event::BLOCK_READY:
+					{
+						commit_block();
+						break;
+					}
+					case frame_event::CHECKSUM_MISMATCH:
+					{
+						log_warning("checksum mismatch at: {0}", i);
+						undo_block();
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	controller::frame_event controller::advance(uint8_t byte)
+	{
+		switch (_state)
+		{
+			case frame_state::HEADER:
+				return handle_header_byte(byte);
+			case frame_state::KEY:
+				return handle_key_byte(byte);
+			case frame_state::VALUE:
+				return handle_value_byte(byte);
+			case frame_state::CHECKSUM:
+				return handle_checksum_byte(byte);
+			case frame_state::DISCARD:
+				return handle_discard(byte);
+		}
+
+		return frame_event::PENDING;
+	}
+
+
+	controller::frame_event controller::handle_header_byte(uint8_t byte)
+	{
+		if (byte != DELIM_CR && byte != DELIM_LF)
+		{
+			_state = frame_state::KEY;
+			_checksum = byte;
+			_key.clear();
+			_key.push_back(static_cast<char>(byte));
+			_value.clear();
+		}
+
+		return frame_event::PENDING;
+	}
+
+	controller::frame_event controller::handle_key_byte(uint8_t byte)
+	{
+		_checksum += byte;
+
+		if (byte == DELIM_TAB)
+		{
+			if (_key == KEY_CHECKSUM)
+			{
+				_state = frame_state::CHECKSUM;
+			}
+			else
+			{
+				_state = frame_state::VALUE;
+				_value.clear();
+			}
+		}
+		else if (byte != DELIM_CR)
+		{
+			_key.push_back(static_cast<char>(byte));
+
+			if (_key.length() > MAX_STRING_LENGTH)
+			{
+				_state = frame_state::DISCARD;
+			}
+		}
+
+		return frame_event::PENDING;
+	}
+
+	controller::frame_event controller::handle_value_byte(uint8_t byte)
+	{
+		_checksum += byte;
+
+		if (byte == DELIM_LF)
+		{
+			_state = frame_state::KEY;
+			return frame_event::PAIR_READY;
+		}
+		
+		if (byte != DELIM_CR)
+		{
+			_value.push_back(static_cast<char>(byte));
+
+			if (_value.length() > MAX_STRING_LENGTH)
+			{
+				_state = frame_state::DISCARD;
+			}
+		}
+
+		return frame_event::PENDING;
+	}
+
+	controller::frame_event controller::handle_checksum_byte(uint8_t byte)
+	{
+		_checksum += byte;
+		_state = frame_state::HEADER;
+
+		if (_checksum != 0)
+		{
+			_checksum = 0;
+			return frame_event::CHECKSUM_MISMATCH;
+		}
+
+		return frame_event::BLOCK_READY;
+	}
+
+	controller::frame_event controller::handle_discard(uint8_t byte)
+	{
+		if (byte == DELIM_LF)
+		{
+			undo_block();
+		}
+
+		return frame_event::PENDING;
+	}
+
+	void controller::parse_pair()
+	{
+		for (const auto& [key, value] : _properties)
+		{
+			if (key == _key)
+			{
+				value->parse(_value);
+				_key.clear();
+				_value.clear();
+				return;
+			}
+		}
+	}
+
+	void controller::commit_block()
+	{
+		for (const auto& [_, value] : _properties)
+		{
+			value->commit();
+		}
+
+		reset();
+	}
+
+	void controller::undo_block()
+	{
+		for (const auto& [_, value] : _properties)
+		{
+			value->undo();
+		}
+
+		reset();
+	}
+
+	void controller::reset()
+	{
+		_state = frame_state::HEADER;
+		_checksum = 0;
+		_key.clear();
+		_value.clear();
+	}
+}
---
+++ src/sykero_mppt.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "sykero_io.hpp"
+#include "sykero_props.hpp"
+
+namespace sl::mppt
+{
+	class controller : public io::file_descriptor
+	{
+	public:
+		controller(const std::filesystem::path& path);
+		~controller() override = default;
+		SL_NON_COPYABLE(controller);
+
+		void update();
+
+		snapshot_average<float, BASE_MILLI> battery_voltage;
+		snapshot_average<float, BASE_MILLI> battery_current;
+		snapshot_average<float, BASE_MILLI> panel_voltage;
+		snapshot_average<float, BASE_NONE> panel_power;
+		snapshot_average<float, BASE_MILLI> load_current;
+
+		snapshot<int, BASE_NONE> state;
+		snapshot<int, BASE_NONE> error;
+		snapshot<int, BASE_NONE> tracker_mode;
+
+		snapshot<float, BASE_CENTI> yield_total;
+		snapshot<int, BASE_NONE> max_power_today;
+
+	private:
+		enum class frame_state
+		{
+			HEADER,
+			KEY,
+			VALUE,
+			CHECKSUM,
+			DISCARD
+		};
+
+		enum class frame_event
+		{
+			PENDING,
+			PAIR_READY,
+			BLOCK_READY,
+			CHECKSUM_MISMATCH
+		};
+
+		frame_event advance(uint8_t byte);
+		
+		frame_event handle_header_byte(uint8_t byte);
+		frame_event handle_key_byte(uint8_t byte);
+		frame_event handle_value_byte(uint8_t byte);
+		frame_event handle_checksum_byte(uint8_t byte);
+		frame_event handle_discard(uint8_t byte);
+
+		void parse_pair();
+		void commit_block();
+		void undo_block();
+		void reset();
+
+		std::array<std::pair<std::string, property*>, 10> _properties;
+		frame_state _state = frame_state::HEADER;
+		std::string _key;
+		std::string _value;
+		uint8_t _checksum = 0;
+	};
+}
