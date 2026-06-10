2d5fe80656bcb3f513eb8da26058dd3ed9be8aa5
visuve <veli-matti.visuri@iki.fi>
2026-06-10 11:06:36 +0300

    Make a property_base class to reduce copy-paste

--- src/sykero_props.hpp
+++ src/sykero_props.hpp
@@ -8,36 +8,35 @@ namespace sl
 	constexpr uint32_t BASE_MILLI = 1000;
 	constexpr uint32_t BASE_MICRO = 1'000'000;
 
-	template<typename T>
-	concept ARITHMETIC = std::is_arithmetic_v<T>;
-
 	class property
 	{
 	public:
-		virtual property& parse(std::string_view value) = 0;
+		virtual property& parse(std::string_view) = 0;
 		virtual void commit() = 0;
 		virtual void undo() = 0;
 		virtual void reset() = 0;
 		virtual ~property() = default;
-	protected:
-		mutable std::recursive_mutex _mutex;
 	};
 
-	// A moving average property that maintains a circular buffer of the last N samples and calculates the average on demand.
-	template<size_t N, ARITHMETIC T, uint32_t DIVIDER = BASE_NONE>
-	class rolling_average : public property
+	template<typename T>
+	concept arithmetic = std::is_arithmetic_v<T>;
+
+	template <arithmetic T, uint32_t DIVIDER = BASE_NONE>
+	class property_base : public property
 	{
 	public:
-		static_assert(N > 0, "N must be greater than zero");
 		static_assert(DIVIDER > 0, "DIVIDER must be greater than zero");
 
+		virtual T get() = 0;
+		virtual void update(T) = 0;
+
 		property& parse(std::string_view value) override
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
+		{ 
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
 			T parsed = static_cast<T>(0);
 			auto [ptr, ec] = std::from_chars(value.data(), value.data() + value.size(), parsed);
-			
+
 			if (ec == std::errc())
 			{
 				_stage = parsed / static_cast<T>(DIVIDER);
@@ -48,7 +47,7 @@ namespace sl
 
 		void commit() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
 			if (_stage.has_value())
 			{
@@ -59,15 +58,25 @@ namespace sl
 
 		void undo() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_stage.reset();
 		}
 
-		void update(T val)
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
+	protected:
+		std::recursive_mutex _mutex;
+		std::optional<T> _stage;
+	};
 
+	// A moving average property that maintains a circular buffer of the last N samples and calculates the average on demand.
+	template<size_t N, arithmetic T, uint32_t DIVIDER = BASE_NONE>
+	class rolling_average : public property_base<T, DIVIDER>
+	{
+	public:
+		static_assert(N > 0, "N must be greater than zero");
+
+		void update(T val) override
+		{
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_buffer[_head] = val;
 			_head = (_head + 1) % N;
 			
@@ -77,9 +86,9 @@ namespace sl
 			}
 		}
 
-		T get() const
+		T get() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
 			if (!_count)
 			{
@@ -96,75 +105,38 @@ namespace sl
 			return sum / static_cast<T>(_count);
 		}
 
-		void reset()
+		void reset() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_buffer.fill(T(0));
 			_head = 0;
 			_count = 0;
-			_stage.reset();
+			this->_stage.reset();
 		}
 
 	private:
 		std::array<T, N> _buffer;
 		size_t _head = 0;
 		size_t _count = 0;
-		std::optional<T> _stage;
 	};
 
 	// A sample average property that maintains a running sum and count of samples, and calculates the average on demand. The average is reset after each get() call.
-	template <ARITHMETIC T, uint32_t DIVIDER = BASE_NONE>
-	class snapshot_average : public property
+	template <arithmetic T, uint32_t DIVIDER = BASE_NONE>
+	class snapshot_average : public property_base<T, DIVIDER>
 	{
 	public:
 		static_assert(DIVIDER > 0, "DIVIDER must be greater than zero");
 
-		property& parse(std::string_view value) override
+		void update(T val) override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			T parsed = static_cast<T>(0);
-
-			auto [ptr, ec] = std::from_chars(value.data(), value.data() + value.size(), parsed);
-			
-			if (ec == std::errc())
-			{
-				_stage = parsed / static_cast<T>(DIVIDER);
-			}
-
-			return *this;
-		}
-
-		void commit() override
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			if (_stage.has_value())
-			{
-				update(_stage.value());
-				_stage.reset();
-			}
-		}
-
-		void undo() override
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			_stage.reset();
-		}
-
-		void update(T val)
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_sum += val;
 			_count++;
 		}
 
-		T get()
+		T get() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
 			if (!_count)
 			{
@@ -178,87 +150,48 @@ namespace sl
 			return _last;
 		}
 
-		void reset()
+		void reset() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-			
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_sum = static_cast<T>(0);
 			_count = 0;
 			_last = static_cast<T>(0);
-			_stage.reset();
+			this->_stage.reset();
 		}
 
 	private:
 		T _sum = static_cast<T>(0);
 		size_t _count = 0;
 		T _last = static_cast<T>(0);
-		std::optional<T> _stage;
 	};
 
 	// A snapshot property that stores the most recently parsed value and only updates the current value when commit() is called. The undo() function discards the snapshot.
-	template <ARITHMETIC T, uint32_t DIVIDER = BASE_NONE>
-	class snapshot : public property
+	template <arithmetic T, uint32_t DIVIDER = BASE_NONE>
+	class snapshot : public property_base<T, DIVIDER>
 	{
 	public:
 		static_assert(DIVIDER > 0, "DIVIDER must be greater than zero");
 
-		property& parse(std::string_view value) override
+		void update(T val) override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			T parsed = static_cast<T>(0);
-			auto [ptr, ec] = std::from_chars(value.data(), value.data() + value.size(), parsed);
-			
-			if (ec == std::errc())
-			{
-				_stage = parsed / static_cast<T>(DIVIDER);
-			}
-
-			return *this;
-		}
-
-		void commit() override
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			if (_stage.has_value())
-			{
-				update(_stage.value());
-				_stage.reset();
-			}
-		}
-
-		void undo() override
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
-			_stage.reset();
-		}
-
-		void update(T val)
-		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_value = val;
 		}
 
-		T get() const
+		T get() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			return _value;
 		}
 
-		void reset()
+		void reset() override
 		{
-			std::lock_guard<std::recursive_mutex> lock(_mutex);
-
+			std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 			_value = static_cast<T>(0);
-			_stage.reset();
+			this->_stage.reset();
 		}
 
 	private:
 		T _value = static_cast<T>(0);
-		std::optional<T> _stage;
 	};
 }
